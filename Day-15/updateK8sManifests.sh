5cfbaaa472bc524b9538dcaf6ca8849f213723db
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2024-02-14 10:34:12 +0530

    update day-15 files

---
+++ Day-15/updateK8sManifests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set the repository URL
+REPO_URL="<repository_url>"
+
+# Set the personal access token
+ACCESS_TOKEN="<your_access_token>"
+
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp_repo
+
+# Navigate into the cloned repository directory
+cd /tmp/temp_repo
+
+# Make changes to the Kubernetes manifest file(s)
+# For example, let's say you want to change the image tag in a deployment.yaml file
+sed -i "s|image:[^$]*|abhishekazurecicd.azurecr.io/$2:$3|g" $1-deployment.yaml
+
+# Add the modified files
+git add .
+
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
+
+# Push the changes back to the repository
+git push
+
+# Cleanup: remove the temporary directory
+rm -rf /tmp/temp_repo
